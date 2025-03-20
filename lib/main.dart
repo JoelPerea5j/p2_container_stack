0e47cee348c39cc67a62f40136fa4c08604776b8
JOEL_ALBERTO_PEREA_CASTORENA <a.22308051281272@cbtis128.edu.mx>
2025-03-20 19:00:09 +0000

    Ejemplo de Card Column

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,63 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false, // Oculta el banner de debug
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Ejemplo de Card con Columna',
+            style: TextStyle(color: Colors.black), // Texto negro
+          ),
+          backgroundColor: Colors.blue, // AppBar azul
+          iconTheme: IconThemeData(color: Colors.black), // Iconos negros
+        ),
+        body: Center(
+          child: Card(
+            elevation: 10, // Agrega sombra
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20), // Bordes redondeados
+            ),
+            color: Colors.blue[50], // Color de fondo
+            margin: EdgeInsets.all(20), // Margen exterior
+            shadowColor: Color(0xff000000), // Color de la sombra
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Información Personal',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Nombre: Juan',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  Text(
+                    'Apellido: Pérez',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  Text(
+                    'Edad: 25 años',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  Text(
+                    'Color Favorito: Azul',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
